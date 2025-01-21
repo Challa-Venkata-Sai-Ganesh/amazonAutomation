91409509eb55c0bd086c5c24002beecc4bc6d9d0
Challa Venkata Sai Ganesh <137880482+Challa-Venkata-Sai-Ganesh@users.noreply.github.com>
2025-01-21 11:32:05 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,10 +1,1 @@
-details= {
-    "ganesh" : 25,
-    "vinith" : {
-        "fullname" : "Vnith Mungaleri",
-        "Age": 26
-    }
-}
-details["vinith"]["fullname"] = "Vinith psycho na kodaka"
-print(details)
 
